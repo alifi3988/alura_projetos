e01e733432fdcdbbd58a1c0fc15601f2d11c4673
alifi3988 <alifi3988@gmail.com>
2025-05-14 09:51:22 -0300

    Commit Inicial

--- boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
+++ boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
@@ -1,5 +1,8 @@
 package br.com.alura.adopet.api.controller;
 
+import br.com.alura.adopet.api.dto.AprovacaoAdocaoDto;
+import br.com.alura.adopet.api.dto.ReprovacaoAdocaoDto;
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.StatusAdocao;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
@@ -25,9 +28,9 @@ public class AdocaoController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity<String> solicitar(@RequestBody @Valid Adocao adocao) {
+    public ResponseEntity<String> solicitar(@RequestBody @Valid SolicitacaoAdocaoDto dto) {
         try {
-            service.solicitar(adocao);
+            service.solicitar(dto);
             return ResponseEntity.ok("Adoção solicitada com sucesso!");
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Erro ao realizar a solicitação. Detalhe: " + e.getMessage());
@@ -36,9 +39,9 @@ public class AdocaoController {
 
     @PutMapping("/aprovar")
     @Transactional
-    public ResponseEntity<String> aprovar(@RequestBody @Valid Adocao adocao) {
+    public ResponseEntity<String> aprovar(@RequestBody @Valid AprovacaoAdocaoDto dto) {
         try {
-            service.aprovar(adocao);
+            service.aprovar(dto);
             return ResponseEntity.ok("Aprovação realizada com sucesso!");
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Erro ao realizar a aprovação. Detalhe: " + e.getMessage());
@@ -47,9 +50,9 @@ public class AdocaoController {
 
     @PutMapping("/reprovar")
     @Transactional
-    public ResponseEntity<String> reprovar(@RequestBody @Valid Adocao adocao) {
+    public ResponseEntity<String> reprovar(@RequestBody @Valid ReprovacaoAdocaoDto dto) {
         try {
-            service.reprovar(adocao);
+            service.reprovar(dto);
             return ResponseEntity.ok("Reprovação realizada com sucesso!");
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Erro ao realizar a reprovação. Detalhe: " + e.getMessage());
