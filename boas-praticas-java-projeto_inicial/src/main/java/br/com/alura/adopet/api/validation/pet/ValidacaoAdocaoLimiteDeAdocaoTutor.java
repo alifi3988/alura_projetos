869051e9e36b0637f63defb0ffb3e605e71e9932
alifi3988 <alifi3988@gmail.com>
2025-05-15 10:40:19 -0300

    FIX - modificação faltante

--- boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/validation/pet/ValidacaoAdocaoLimiteDeAdocaoTutor.java
+++ boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/validation/pet/ValidacaoAdocaoLimiteDeAdocaoTutor.java
@@ -2,37 +2,19 @@ package br.com.alura.adopet.api.validation.pet;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.exception.ValidacaoException;
-import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.StatusAdocao;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
-import br.com.alura.adopet.api.repository.TutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ValidacaoAdocaoLimiteDeAdocaoTutor implements ValidacaoAdocao {
 
-  @Autowired private TutorRepository tutorRepository;
-
   @Autowired private AdocaoRepository adocaoRepository;
 
   public void validar(SolicitacaoAdocaoDto dto) {
-
-    var adocoes = adocaoRepository.findAll();
-    var tutor = tutorRepository.findById(dto.idTutor()).get();
-
-    // tutor de adocao == tutor
-    // status de adocao == APROVADO
-    // a quantidade de docoes desse tutor tem que ser <= 5
-
-    for (Adocao a : adocoes) {
-      int contador = 0;
-      if (a.getTutor() == tutor && a.getStatus() == StatusAdocao.APROVADO) {
-        contador = contador + 1;
-      }
-      if (contador == 5) {
-        throw new ValidacaoException("Tutor chegou ao limite máximo de 5 adoções!");
-      }
+    if (adocaoRepository.findByTutorIdAndStatus(dto.idTutor(), StatusAdocao.APROVADO).size() == 5) {
+      throw new ValidacaoException("Tutor chegou ao limite máximo de 5 adoções!");
     }
   }
 }
