5ce44b7de0a5dc3dc774038de1448fb42d70879d
alifi3988 <alifi3988@gmail.com>
2025-06-24 23:10:16 -0300

    Update AbrigoServiceTest.java

--- boas-praticas-java-projeto_inicial/src/test/java/br/com/alura/adopet/api/service/AbrigoServiceTest.java
+++ boas-praticas-java-projeto_inicial/src/test/java/br/com/alura/adopet/api/service/AbrigoServiceTest.java
@@ -1,5 +1,9 @@
 package br.com.alura.adopet.api.service;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+
 import br.com.alura.adopet.api.dto.abrigo.AbrigoDto;
 import br.com.alura.adopet.api.dto.abrigo.CadastroAbrigoDto;
 import br.com.alura.adopet.api.dto.pet.PetDto;
@@ -13,14 +17,13 @@ import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class AbrigoServiceTest {
+public class AbrigoServiceTest {
 
   @Mock private AbrigoRepository abrigoRepositoryMock;
 
@@ -41,7 +44,7 @@ class AbrigoServiceTest {
   @InjectMocks private AbrigoService abrigoService;
 
   @Test
-  void deve_listar_todos_abrigos() {
+  public void deve_listar_todos_abrigos() {
     Mockito.when(abrigoRepositoryMock.findAll()).thenReturn(listaAbrigoMock);
     List<AbrigoDto> retorno = abrigoService.listarTodosAbrigos();
 
@@ -49,20 +52,16 @@ class AbrigoServiceTest {
   }
 
   @Test
-  void deve_cadastrar_abrigo() {
-    Mockito.when(
-            abrigoRepositoryMock.existsByNomeOrTelefoneOrEmail(
-                ArgumentMatchers.any(), ArgumentMatchers.any(), ArgumentMatchers.any()))
+  public void deve_cadastrar_abrigo() {
+    Mockito.when(abrigoRepositoryMock.existsByNomeOrTelefoneOrEmail(any(), any(), any()))
         .thenReturn(false);
 
     Assertions.assertDoesNotThrow(() -> abrigoService.cadastrarAbrigo(cadastroAbrigoDtoMock));
   }
 
   @Test
-  void deve_acionar_uma_exception_ao_tentar_cadastrar_abrigo() {
-    Mockito.when(
-            abrigoRepositoryMock.existsByNomeOrTelefoneOrEmail(
-                ArgumentMatchers.any(), ArgumentMatchers.any(), ArgumentMatchers.any()))
+  public void deve_acionar_uma_exception_ao_tentar_cadastrar_abrigo() {
+    Mockito.when(abrigoRepositoryMock.existsByNomeOrTelefoneOrEmail(any(), any(), any()))
         .thenReturn(true);
 
     ValidacaoException exceptionLancada =
@@ -73,7 +72,7 @@ class AbrigoServiceTest {
   }
 
   @Test
-  void deve_listar_pets_por_nome() {
+  public void deve_listar_pets_por_nome() {
     String nomeAbrigo = "teste";
     Mockito.when(abrigoRepositoryMock.findByNome(nomeAbrigo)).thenReturn(abrigoOptionalMock);
     Mockito.when(petRepositoryMock.findByAbrigo(abrigoOptionalMock.get())).thenReturn(listaPetMock);
@@ -83,7 +82,7 @@ class AbrigoServiceTest {
   }
 
   @Test
-  void deve_listar_pets_por_id() {
+  public void deve_listar_pets_por_id() {
     String idAbrigo = "123456";
     Mockito.when(abrigoRepositoryMock.findById(Long.parseLong(idAbrigo)))
         .thenReturn(abrigoOptionalMock);
@@ -94,7 +93,7 @@ class AbrigoServiceTest {
   }
 
   @Test
-  void deve_acionar_exception_pets_por_nome() {
+  public void deve_acionar_exception_ao_listar_pets_por_nome() {
     String nomeAbrigo = "teste";
     Mockito.when(abrigoRepositoryMock.findByNome(nomeAbrigo)).thenReturn(Optional.empty());
 
@@ -106,7 +105,7 @@ class AbrigoServiceTest {
   }
 
   @Test
-  void deve_acionar_exception_pets_por_id() {
+  public void deve_acionar_exception_pets_por_id() {
     String idAbrigo = "123456";
     Mockito.when(abrigoRepositoryMock.findById(Long.parseLong(idAbrigo)))
         .thenReturn(Optional.empty());
@@ -116,19 +115,32 @@ class AbrigoServiceTest {
             ValidacaoException.class, () -> abrigoService.listarPetsPorIdOrNomeDoAbrigo(idAbrigo));
     Assertions.assertEquals("Abrigo não encontrado", execaoLancada.getMessage());
   }
-  // TODO: Preciso revisar esse teste, de acordo com o vídeo, pois não vi como preciso fazer. Esta
-  // gerandp erro
-  //    @Test
-  //    void deve_cadastrar_pet_pelo_nome_do_abrigo() {
-  //        String nomeAbrigo = "teste";
-  //
-  // Mockito.when(abrigoRepositoryMock.findByNome(nomeAbrigo)).thenReturn(abrigoOptionalMock);
-  //
-  // Mockito.when(petRepositoryMock.findByAbrigo(abrigoOptionalMock.get())).thenReturn(listaPetMock);
-  //
-  //        Mockito.verify(abrigoOptionalMock.get(), times(1)).adicionarPetEmAbrigo(petMock);
-  //        Mockito.verify(abrigoRepositoryMock, times(1)).save(abrigoOptionalMock.get());
-  //
-  //        abrigoService.cadastrarPet(nomeAbrigo, petMock);
-  //    }
+
+  @Test
+  public void deve_cadastrar_pet_pelo_nome_do_abrigo() {
+    String nomeAbrigo = "teste";
+
+    Mockito.when(abrigoRepositoryMock.findByNome(nomeAbrigo)).thenReturn(Optional.of(abrigoMock));
+
+    Assertions.assertDoesNotThrow(() -> abrigoService.cadastrarPet(nomeAbrigo, petMock));
+
+    Mockito.verify(petMock, times(1)).adicionarAbrigoEmPet(abrigoMock);
+    Mockito.verify(abrigoMock, times(1)).adicionarPetEmAbrigo(petMock);
+    Mockito.verify(abrigoRepositoryMock, times(1)).save(abrigoMock);
+  }
+
+  @Test
+  public void deve_lancar_excecao_quando_abrigo_nao_encontrado() {
+    String nomeAbrigoInexistente = "naoExiste";
+
+    Mockito.when(abrigoRepositoryMock.findByNome(nomeAbrigoInexistente))
+        .thenReturn(Optional.empty());
+
+    Assertions.assertThrows(
+        ValidacaoException.class, () -> abrigoService.cadastrarPet(nomeAbrigoInexistente, petMock));
+
+    Mockito.verify(abrigoRepositoryMock, never()).save(any());
+    Mockito.verify(petMock, never()).adicionarAbrigoEmPet(any());
+    Mockito.verify(abrigoMock, never()).adicionarPetEmAbrigo(any());
+  }
 }
